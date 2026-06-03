b87f738001c301f279aa0fa68f54ebbca95ec356
pa_kAJ <pankaj4utanda@gmail.com>
2026-06-03 23:35:57 +0530

    fixBuild

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col bg-red text-white">{children}</body>
+      <body className="min-h-full flex flex-col text-white">{children}</body>
     </html>
   );
 }
