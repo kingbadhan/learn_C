1e8b368e5ce3eb7704ee9549b5258ac06d996489
pa_kAJ <pankaj4utanda@gmail.com>
2026-07-20 23:57:09 +0530

    short site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Hero from "@/sections/hero";
-import Projects from "@/sections/projects";
+// import Projects from "@/sections/projects";
 import Contact from "@/sections/contact";
 
 export default function Home() {
   return (
     <main className="h-screen overflow-y-scroll snap-y snap-mandatory scroll-smooth bg-background">
       <Hero />
-      <Projects />
+      {/* <Projects /> */}
       <Contact />
     </main>
   );
